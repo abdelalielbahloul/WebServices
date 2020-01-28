4e943fc9d9837f3ad4c18a80bbff3e713f987b1b
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-01-28 17:33:08 +0100

    add appsettings to .gitignore file

--- Services/SendMailsApi.cs
+++ Services/SendMailsApi.cs
@@ -11,8 +11,7 @@ namespace WebServices.Services
     {
         public static async Task<bool> Execute(string userEmail, string userName, string subject, string plainTextContent, string htmlContent)
         {
-            //var apiKey = Environment.GetEnvironmentVariable("apiKey");
-            var apiKey = "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";
+            var apiKey = Environment.GetEnvironmentVariable("apiKey");
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("test@example.com", "Abdelali EL BAHLOUL");
             var to = new EmailAddress(userEmail, userName);
